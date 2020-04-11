606b3cd5930306df72a23896b7029809130e91f3
Vinícius David Fonseca <59883050+vinicius-david@users.noreply.github.com>
2020-04-10 21:43:03 -0300

    Update server.js

--- Modulo_4/server.js
+++ Modulo_4/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+const methodOverride = require('method-override');
 
 const server = express();
 
@@ -8,6 +9,7 @@ server.set("view engine", "njk")
 
 server.use(express.urlencoded({ extended:true }))
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
 server.use(routes)
 
 nunjucks.configure("views", {
