731d210caf3a949abfbe2ee4fc090d11713cefd1
Vinícius David Fonseca <59883050+vinicius-david@users.noreply.github.com>
2020-04-12 18:00:41 -0300

    Add files via upload

---
+++ Modulo_4/util.js
@@ -0,0 +1,34 @@
+module.exports = {
+  calcularIdade: function(timestamp) {
+
+    const hoje = new Date()
+    const nascimento = new Date(timestamp)
+  
+    let idade = hoje.getFullYear() - nascimento.getFullYear()
+    const mes = hoje.getMonth() - nascimento.getMonth()
+  
+    if (mes < 0 || (mes == 0 && hoje.getDate() <= nascimento.getDate()) ) {
+      idade = idade -1
+    }
+  
+    return idade
+  },
+
+  calcularData: function(timestamp){
+
+    const data = new Date(timestamp)
+
+    const ano = data.getUTCFullYear()
+    const mes = `0${data.getUTCMonth() + 1}`.slice(-2)
+    const dia = `0${data.getUTCDate()}`.slice(-2)
+
+    return {
+      dia,
+      mes,
+      ano,
+      iso: `${ano}-${mes}-${dia}`,
+      diaNascimento: `${dia}/${mes}`
+    }
+
+  }
+}
