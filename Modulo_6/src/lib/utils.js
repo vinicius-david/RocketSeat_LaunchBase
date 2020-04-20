c36fbbb4bb975b7f2e8297ca2bde12375322f3a3
vinicius-david <vinicius.davidf@gmail.com>
2020-04-20 16:27:17 -0300

    commit inicial

---
+++ Modulo_6/src/lib/utils.js
@@ -0,0 +1,35 @@
+module.exports = {
+  calcularIdade(timestamp) {
+
+    const hoje = new Date()
+    const nascimento = new Date(timestamp)
+  
+    let idade = hoje.getFullYear() - nascimento.getFullYear()
+    const mes = hoje.getMonth() - nascimento.getMonth()
+  
+    if (mes < 0 || (mes == 0 && hoje.getDate() <= nascimento.getDate()) ) {
+      idade = idade -1
+    }
+  
+    return idade
+  },
+
+  calcularData(timestamp){
+
+    const data = new Date(timestamp)
+
+    const ano = data.getUTCFullYear()
+    const mes = `0${data.getUTCMonth() + 1}`.slice(-2)
+    const dia = `0${data.getUTCDate()}`.slice(-2)
+
+    return {
+      dia,
+      mes,
+      ano,
+      iso: `${ano}-${mes}-${dia}`,
+      diaNascimento: `${dia}/${mes}`,
+      format: `${dia}-${mes}-${ano}`
+    }
+
+  }
+}
