d0e5d28267fa79a8fe08baf68b3ae8921e5bdf02
vinicius-david <vinicius.davidf@gmail.com>
2020-04-21 17:10:12 -0300

    atualizando e excluindo produtos

--- Modulo_6/src/app/controllers/ProductController.js
+++ Modulo_6/src/app/controllers/ProductController.js
@@ -1,5 +1,6 @@
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const { formatPrice } = require('../../lib/utils')
 
 module.exports = {
   create(req, res) {
@@ -29,10 +30,49 @@ module.exports = {
     let results = await Product.create(req.body)
     const productId = results.rows[0].id
 
+    return res.redirect(`products/${productId}`)
+
+  },
+  async edit(req, res) {
+
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if (!product) return res.send('Produto não encontrado')
+
+    product.old_price = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+
     results = await Category.all()
     const categories = results.rows
 
-    return res.render('products/create.njk', { productId, categories })
+    return res.render('products/edit.njk', { product, categories })
+  },
+  async put(req, res) {
+    const keys = Object.keys(req.body)
 
+    for (key of keys) {
+      if ( req.body[key] == '' ) {
+        return res.send('Por favor, preencha todos os campos')
+      }
+    }
+
+    req.body.price = req.body.price.replace(/\D/g,"")
+
+    if (req.body.old_price != req.body.price) {
+      const oldProduct = await Product.find(req.body.id)
+
+      req.body.old_price = oldProduct.rows[0].price
+    }
+
+    await Product.update(req.body)
+
+    return res.redirect(`/products/${req.body.id}/edit`)
+  },
+  async delete(req, res) {
+
+    await Product.delete(req.body.id)
+
+    return res.redirect('/products/create')
   }
 }
--- Modulo_6/src/lib/utils.js
+++ Modulo_6/src/lib/utils.js
@@ -31,5 +31,11 @@ module.exports = {
       format: `${dia}-${mes}-${ano}`
     }
 
+  },
+  formatPrice(price) {
+    return new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price/100)
   }
 }
