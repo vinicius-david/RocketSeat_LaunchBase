36533efe6ecd5c343060e513ef1be8674eb9f66d
vinicius-david <vinicius.davidf@gmail.com>
2020-04-21 13:24:52 -0300

    cadastro de produtos

--- Modulo_6/src/routes.js
+++ Modulo_6/src/routes.js
@@ -8,6 +8,7 @@ routes.get('/', function(req, res) {
 })
 
 routes.get('/products/create', ProductController.create)
+routes.post('/products', ProductController.post)
 
 routes.get('/ads/create', function(req, res) {
   return res.render("products/create.njk")
