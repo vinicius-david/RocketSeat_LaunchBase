57652710308a5364a01d44e9ced32a13df8645dc
vinicius-david <vinicius.davidf@gmail.com>
2020-04-22 18:18:48 -0300

    feature: excluir imagens

--- Modulo_6_7/src/app/controllers/ProductController.js
+++ Modulo_6_7/src/app/controllers/ProductController.js
@@ -51,18 +51,39 @@ module.exports = {
     product.old_price = formatPrice(product.old_price)
     product.price = formatPrice(product.price)
 
+    // get categories
+
     results = await Category.all()
     const categories = results.rows
 
-    return res.render('products/edit.njk', { product, categories })
+    //get images
+
+    results = await Product.files(product.id)
+    let files = results.rows
+    files = files.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
+    }))
+
+    return res.render('products/edit.njk', { product, categories, files })
   },
   async put(req, res) {
     const keys = Object.keys(req.body)
 
     for (key of keys) {
-      if ( req.body[key] == '' ) {
+      if ( req.body[key] == '' && key != 'removed_files' ) {
         return res.send('Por favor, preencha todos os campos')
       }
+    }
+
+    if (req.body.removed_files) {
+      const removedFiles = req.body.removed_files.split(',')
+      const lastIndex = removedFiles.length - 1
+      removedFiles.splice(lastIndex, 1)
+
+      const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+      await Promise.all(removedFilesPromise)
     }
 
     req.body.price = req.body.price.replace(/\D/g,"")
--- Modulo_6_7/src/app/models/File.js
+++ Modulo_6_7/src/app/models/File.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db')
+const fs = require('fs')
 
 module.exports = {
   create({filename, path, product_id}) {
@@ -19,5 +20,22 @@ module.exports = {
     ]
 
     return db.query(query, values)
+  },
+  async delete(id) {
+
+    try{
+
+      const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
+      const file = result.rows[0]
+  
+      fs.unlinkSync(file.path)
+
+    }catch(err) {
+      console.error(err)
+    }
+
+    return db.query(`
+      DELETE FROM files WHERE id = $1
+    `, [id])
   }
 }
