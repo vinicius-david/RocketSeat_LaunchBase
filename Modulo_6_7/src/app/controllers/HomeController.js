76cd4cad1f9be39d851678b884f171cb2ec1e26a
vinicius-david <vinicius.davidf@gmail.com>
2020-04-30 13:50:36 -0300

    página de busca

--- Modulo_6_7/src/app/controllers/HomeController.js
+++ Modulo_6_7/src/app/controllers/HomeController.js
@@ -1,5 +1,4 @@
 const Product = require('../models/Product')
-const File = require('../models/File')
 
 const { formatPrice } = require('../../lib/utils')
 
--- Modulo_6_7/src/routes.js
+++ Modulo_6_7/src/routes.js
@@ -3,18 +3,27 @@ const routes = express.Router();
 
 const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
-const HomeControlles = require('./app/controllers/HomeController')
+const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
-routes.get('/', HomeControlles.index)
+// HOME
+routes.get('/', HomeController.index)
 
-routes.get('/products/:id', ProductController.show)
+//SEARCH
+routes.get('/products/search', SearchController.index)
+
+// PRODUCT
 routes.get('/products/create', ProductController.create)
+routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
 
 routes.post('/products', multer.array('photos', 6), ProductController.post)
 routes.put('/products', multer.array('photos', 6), ProductController.put)
 routes.delete('/products', ProductController.delete)
 
+
+
+// ALIAS
 routes.get('/ads/create', ProductController.create)
 
 
