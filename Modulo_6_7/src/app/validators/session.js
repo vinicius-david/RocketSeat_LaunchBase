be011205b226b6520386742cfae7e56465e4a4cb
vinicius-david <vinicius.davidf@gmail.com>
2020-05-02 15:51:21 -0300

    login e logout de usuários

---
+++ Modulo_6_7/src/app/validators/session.js
@@ -0,0 +1,27 @@
+const User = require('../models/User')
+const { compare } = require('bcryptjs')
+
+module.exports = {
+  async login(req, res, next) {
+
+    let { email, password } = req.body
+    
+    const user = await User.findOne({ where: {email} })
+
+    if (!user) return res.render('session/login', {
+      user: req.body,
+      error: 'Usuário não cadastrado'
+    })
+
+    const passed = await compare(password, user.password)
+
+    if (!passed) return res.render('session/login', {
+      user: req.body,
+      error: 'Senha incorreta'
+    })
+
+    req.user = user
+
+    next()
+  }
+}
